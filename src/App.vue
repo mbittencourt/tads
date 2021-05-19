2247005e3968a8c9197db4dbc57450b4d13cfedf
Messias Bittencourt <messias.b@gmail.com>
2021-05-19 19:58:10 -0300

    Aula 2 - Add Grid

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <Grid msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Grid from './components/Grid.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Grid
   }
 }
 </script>
---
+++ src/components/Grid.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="hello">
+    <p>
+          <b-table 
+            striped hover 
+            :items="dto"  
+            :fields="campos"
+            :per-page="registroPorPagina"
+            :current-page="paginaAtual"
+          />
+
+
+          <b-pagination
+            v-model="paginaAtual"
+            :total-rows="rows"
+            :per-page="registroPorPagina"
+            aria-controls="my-table"
+          ></b-pagination>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  data(){
+    return {
+      registroPorPagina: 3,
+      paginaAtual: 1,
+
+      campos: [
+          {
+            key: 'nome',
+            sortable: true
+          },
+          {
+            key: 'idade',
+            sortable: true
+          }
+      ],
+      dto: [
+            {
+              "nome": "Henrique",
+              "idade": 58
+            },
+            {
+              "nome": "Charles",
+              "idade": 39
+            },
+            {
+              "nome": "Keven",
+              "idade": 47
+            },
+            {
+              "nome": "Messias",
+              "idade": 18
+            },
+            {
+              "nome": "Gustavo",
+              "idade": 57
+            },
+            {
+              "nome": "Carlos Augusto",
+              "idade": 48
+            },
+            {
+              "nome": "Natalia",
+              "idade": 18
+            }
+          ]
+    }
+  },
+  
+  methods: {
+    
+  },
+
+  computed: {
+      rows() {
+        return this.dto.length
+      }
+    }
+}
+</script>
